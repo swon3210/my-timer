d740a181a1c5da5acc8827be6a05f3f6ebd61a9f
swon3210 <swon3210@gmail.com>
2024-10-15 23:29:37 +0900

    API 요청값을 localStorage 에 캐시하여 응답값을 불러오기 전에 렌더링을 수행할 수 있도록 수정

---
+++ src/app/api/firebase.ts
@@ -0,0 +1,147 @@
+import { initializeApp } from "firebase/app";
+import {
+  getStorage,
+  ref as storageRef,
+  listAll,
+  getDownloadURL,
+} from "firebase/storage";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase, ref as dbRef, set, get } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeylqDbJ872XSsURgrRKbCcExj_sVlkqY",
+  authDomain: "my-timer-12943.firebaseapp.com",
+  databaseURL:
+    "https://my-timer-12943-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my-timer-12943",
+  storageBucket: "my-timer-12943.appspot.com",
+  messagingSenderId: "517814501841",
+  appId: "1:517814501841:web:2082fb3ef4954b36b7a0f0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app, "gs://my-timer-12943.appspot.com");
+
+export async function getFolderList(path: string) {
+  const folderRef = storageRef(storage, path);
+
+  try {
+    // 'storagePath' 내 모든 파일 및 하위 폴더 목록 가져오기
+    const res = await listAll(folderRef);
+
+    // 폴더에 해당하는 prefix 목록을 가져옴 (폴더는 prefix로 구분)
+    const folders = res.prefixes.map((folderRef) => folderRef.fullPath);
+
+    return folders;
+  } catch (error) {
+    console.error("Error fetching folder list:", error);
+  }
+}
+
+export async function getImageListFromFolder(path: string) {
+  const folderRef = storageRef(storage, path);
+
+  try {
+    // 폴더 내 모든 파일 목록 가져오기
+    const res = await listAll(folderRef);
+
+    // 이미지 파일들의 다운로드 URL 가져오기
+    const imageUrls = await Promise.all(
+      res.items.map((itemRef) => getDownloadURL(itemRef))
+    );
+    return imageUrls;
+  } catch (error) {
+    console.error("Error fetching image list:", error);
+  }
+}
+
+const auth = getAuth(app);
+
+export const signUp = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("회원가입 성공:", user);
+    return user;
+  } catch (error) {
+    console.error("회원가입 실패:", error);
+    throw error;
+  }
+};
+
+// 로그인 함수
+export const signIn = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("로그인 성공:", user);
+    return user;
+  } catch (error) {
+    console.error("로그인 실패:", error);
+    throw error;
+  }
+};
+
+const database = getDatabase(app);
+
+export type AppSettings = {
+  shouldExposeTimer: boolean;
+  tickSeconds: number;
+  selectedBGM: string;
+  selectedVoice: string;
+};
+
+export const saveSettings = async (
+  userId: string,
+  settings: AppSettings
+): Promise<void> => {
+  try {
+    const settingsRef = dbRef(database, `users/${userId}/settings`);
+    await set(settingsRef, settings);
+  } catch (error) {
+    console.error("Settings 정보를 저장하는데 실패하였습니다 : ", error);
+  }
+};
+
+export const getSettings = async (userId: string) => {
+  try {
+    const settingsRef = dbRef(database, `users/${userId}/settings`);
+    const snapshot = await get(settingsRef);
+
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Settings 정보를 불러오는데 실패하였습니다 : ", error);
+    return null;
+  }
+};
+
+export default app;
--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,10 +1,29 @@
 "use client";
 
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
+// TODO : 정확한 동작 원리와 사용법 파악
+const persister = createSyncStoragePersister({
+  storage: typeof window !== "undefined" ? window.localStorage : undefined,
+});
 
 export function QueryProvider({ children }: React.PropsWithChildren) {
-  const [client] = useState(new QueryClient());
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          gcTime: 1000 * 60 * 60 * 24, // 24 hours
+        },
+      },
+    })
+  );
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <PersistQueryClientProvider client={client} persistOptions={{ persister }}>
+      {children}
+    </PersistQueryClientProvider>
+  );
 }
