27ce457ae812da8c4f9b246bbf7b4c462c52972b
swon3210 <swon3210@gmail.com>
2024-11-26 23:05:01 +0900

    chore: 숨겨야하는 secret 을 환경변수에 삽입

--- src/app/api/firebase-admin.ts
+++ src/app/api/firebase-admin.ts
@@ -1,8 +1,23 @@
 import admin from "firebase-admin";
-import serviceAccount from "./service-account.json";
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import type { DecodedIdToken } from "firebase-admin/auth";
+
+const serviceAccount = {
+  type: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:
+    process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_UNIVERSE_DOMAIN,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
--- src/app/api/firebase.ts
+++ src/app/api/firebase.ts
@@ -11,14 +11,13 @@ import { getDatabase, ref as dbRef, set, get } from "firebase/database";
 import { AppSettings } from "@/lib/types";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeylqDbJ872XSsURgrRKbCcExj_sVlkqY",
-  authDomain: "my-timer-12943.firebaseapp.com",
-  databaseURL:
-    "https://my-timer-12943-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "my-timer-12943",
-  storageBucket: "my-timer-12943.appspot.com",
-  messagingSenderId: "517814501841",
-  appId: "1:517814501841:web:2082fb3ef4954b36b7a0f0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
