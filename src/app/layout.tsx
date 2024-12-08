0b519e598c8005493242bbdb00080afac6c13329
swon3210 <swon3210@gmail.com>
2024-12-08 09:33:14 +0900

    feat: 비회원 접근 리디렉션 로직 임시 비활성화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 import { QueryProvider } from "@/lib/providers";
 import { Toaster } from "sonner";
 import FirebaseProvider from "@/providers/FirebaseProvider";
-import { checkAuth } from "@/domains/users/fetchers";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
+// import { checkAuth } from "@/domains/users/fetchers";
+// import { redirect } from "next/navigation";
+// import { headers } from "next/headers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,14 +29,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = headers().get("x-pathname") || "";
+  // const pathname = headers().get("x-pathname") || "";
 
-  const { success } = await checkAuth();
-  const isAuthPage = ["/sign-in", "/sign-up"].includes(pathname);
+  // const { success } = await checkAuth();
+  // const isAuthPage = ["/sign-in", "/sign-up"].includes(pathname);
 
-  if (!success && !isAuthPage) {
-    redirect("/sign-in");
-  }
+  // if (!success && !isAuthPage) {
+  //   redirect("/sign-in");
+  // }
 
   return (
     <html lang="ko-kr">
