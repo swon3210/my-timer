6ffd1a21cb4c4bc6e921993ea5333c11e5993460
swon3210 <swon3210@gmail.com>
2025-03-25 00:48:11 +0900

    feat: 폴더 이름 표기 로직 추가

--- src/app/gallery-timer/layout.tsx
+++ src/app/gallery-timer/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import BackButton from "@/components/BackButton";
 import { Button } from "@/components/ui/button";
+import { folderNameAtom } from "@/lib/atoms";
+import { useAtom, useAtomValue } from "jotai";
 import { FolderSearch, Settings } from "lucide-react";
 import Link from "next/link";
 
@@ -8,10 +12,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const folderName = useAtomValue(folderNameAtom);
+
   return (
     <main className="w-full h-full max-w-app-container mx-auto">
-      <div className="fixed z-10 top-0 left-0 p-2">
+      <div className="fixed z-10 top-0 left-0 p-2 flex items-center">
         <BackButton className="size-10 mr-2" />
+        <span className="text-sm">
+          {folderName ? decodeURIComponent(folderName) : "Gallery Timer"}
+        </span>
       </div>
       <div className="fixed z-10 top-0 right-0 p-2 flex items-center gap-1">
         <Button variant="ghost" size="icon" className="size-10" asChild>
